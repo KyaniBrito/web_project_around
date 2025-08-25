440d94a365bba1a35049e3511606d42148b359e1
KyaniBrito <kyanimag@icloud.com>
2025-08-25 12:06:23 -0300

    alterna curtidas no cartão

--- components/Card.js
+++ components/Card.js
@@ -3,15 +3,19 @@ export default class Card {
     { card, cardSelector },
     handleCardClick,
     handleDeleteClick,
-    popupConfirmDelete
-    // currentUserId
+    handleLikeClick,
+    userId
   ) {
-    this._card = card;
+    this._card = {
+      ...card,
+      likes: Array.isArray(card.likes) ? card.likes : [],
+      isLiked: card.isLiked ?? false,
+    };
     this._cardSelector = cardSelector;
     this._handleCardClick = handleCardClick;
     this._handleDeleteClick = handleDeleteClick;
-    this._popupConfirmDelete = popupConfirmDelete;
-    // this._currentUserId = currentUserId;
+    this._handleLikeClick = handleLikeClick;
+    this._userId = userId;
   }
 
   _getTemplate() {
@@ -23,11 +27,12 @@ export default class Card {
   }
 
   _setEventListeners() {
-    this._cardElement
-      .querySelector(".elements__card-label-button")
-      .addEventListener("click", (evt) => {
-        evt.target.classList.toggle("elements__card-label-button_active");
-      });
+    this._likeButton = this._cardElement.querySelector(
+      ".elements__card-label-button"
+    );
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeClick(this);
+    });
 
     this._cardElement
       .querySelector(".elements__card-image")
@@ -46,6 +51,32 @@ export default class Card {
       console.log("Card Element:", this._cardElement);
       this._handleDeleteClick(this._card._id, this._cardElement);
     });
+    if (this._card.owner && this._card.owner._id !== this._userId) {
+      deleteButton.remove();
+    }
+  }
+
+  _updateLikeButtonState() {
+    if (!this._likeButton) return;
+
+    if (this._card.isLiked) {
+      this._likeButton.classList.add("elements__card-label-button_active");
+    } else {
+      this._likeButton.classList.remove("elements__card-label-button_active");
+    }
+  }
+
+  updateLikes(updatedCard) {
+    this._card.likes = Array.isArray(updatedCard.likes)
+      ? updatedCard.likes
+      : [];
+    this._card.isLiked = updatedCard.isLiked;
+
+    this._updateLikeButtonState();
+  }
+
+  isLiked() {
+    return this._card.isLiked;
   }
 
   generateCard() {
@@ -61,6 +92,7 @@ export default class Card {
       this._card.name;
 
     this._setEventListeners();
+    this._updateLikeButtonState();
 
     return this._cardElement;
   }
